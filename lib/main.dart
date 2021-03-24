b74611959e1853d410cc629992c5ad0eb42047b2
batuhankrbb <batuhankarababa@gmail.com>
2021-03-24 19:54:06 +0300

    main page added

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,67 @@ import 'package:flutter_currency_app_mobx/views/currency_list_page.dart';
 
 void main() {
   launchGetIt();
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CurrencyListPage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            buttonToCurrency(context),
+            buttonToCounter(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  RaisedButton buttonToCurrency(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return CurrencyListPage();
+            },
+          ),
+        );
+      },
+      color: Colors.purple[300],
+      child: Text(
+        "CURRENCY",
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  RaisedButton buttonToCounter(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return CurrencyListPage();
+            },
+          ),
+        );
+      },
+      color: Colors.teal[300],
+      child: Text(
+        "COUNTER",
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 }
